a7022f17ab7f59300144977036e3eff5ad679b6d
mdt8740 <mattthyng@gmail.com>
2019-02-04 12:50:04 -0500

    Added validation checks to ensure proper formatting of YAML files during migration

--- migration/migrate_2019_02_04_10_41.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../scripts/migration'
-
-def migrate_2019_02_04_10_41
-	insert_lines_dir("../migration/cves_test","CVE: CVE-\\d{4}-\\d+","../migration/test_input.txt")
-end
-
-migrate_2019_02_04_10_41()
