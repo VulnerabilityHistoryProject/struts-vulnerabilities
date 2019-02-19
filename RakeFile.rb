ef96914699e1e007359cd8cbc482ee10a8109ad3
Matt Thyng <mattthyng@gmail.com>
2019-02-19 10:06:47 -0500

    Added repo clone to rakefile

--- RakeFile.rb
+++ RakeFile.rb
@@ -6,4 +6,9 @@ namespace :pull do
 	scraper.crawl
 	scraper.save_cve_ymls
   end
+  task :repo do
+    Dir.chdir('tmp/') do
+      `git clone https://github.com/apache/struts.git`
+    end
+  end
 end
